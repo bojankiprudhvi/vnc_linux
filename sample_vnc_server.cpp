a2d99a432c273c607392bcbb6c8de0b4a27f1c4f
bojankiprudhvi <88262298+bojankiprudhvi@users.noreply.github.com>
2024-11-18 13:33:44 +0530

    updated the code to chnages the fps number when needed

--- sample_vnc_server.cpp
+++ sample_vnc_server.cpp
@@ -10,9 +10,10 @@
 #include <netinet/in.h>
 #include <cstring>
 #include <signal.h>
+#include <filesystem>
 #include <chrono>  // For time management
 #define Status MyStatus // Rename to avoid conflict
-#include <opencv2/opencv.hpp>
+
 #include <X11/Xlib.h>
 #include <X11/extensions/XShm.h>
 #include <X11/Xutil.h>
@@ -84,13 +85,72 @@ public:
     });
         return true;
     }
+ /*void dumpFramebufferToFileWithTimeStamp(const void *fb_addr, size_t size)
+        {
+        static int frame_cnt = 0;
+        if (frame_cnt > 120)
+            return;
+        std::string directory = "/home/prudhviraj/neatvnc_build/example/dump/";
 
+        if (!std::filesystem::exists(directory))
+        {
+            if (!std::filesystem::create_directory(directory))
+            {
+                std::cerr << "Failed to create directory: " << directory << std::endl;
+                return;
+            }
+        }
+        auto now = std::chrono::system_clock::now();
+        auto time_t_now = std::chrono::system_clock::to_time_t(now);
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+
+        std::ostringstream oss;
+        oss << directory << "framebuffer_dump_" << std::put_time(std::localtime(&time_t_now), "%Y%m%d_%H%M%S")
+            << '_' << std::setfill('0') << std::setw(3) << ms.count() << ".raw";
+        std::string fileName = oss.str();
+
+        // Open file and write framebuffer data
+        std::ofstream file(fileName, std::ios::binary);
+        if (!file) {
+            std::cerr << "Failed to open file: " << fileName << std::endl;
+            return;
+        }
+
+        file.write(reinterpret_cast<const char *>(fb_addr), size);
+        if (!file) {
+            std::cerr << "Failed to write data to file: " << fileName << std::endl;
+            return;
+        }
+
+        std::cout << "Saved framebuffer to " << fileName << std::endl;
+        frame_cnt++;
+    }  
+*/
+   /* void dump_from_queue()
+    {
+        while()
+        {
+            //if queue not empyt
+            //wriete the data and free fb
+            //queue pop
+            
+        }
+
+
+    } 
+    void push_to_queue(const uint32_t* data, size_t size,time)
+    {
+        //push the data into queue
+    }*/
     void updateFramebuffer(const uint32_t* data, size_t size) {
         if (!data || size != width * height * BYTES_PER_PIXEL) {
             std::cerr << "Framebuffer size mismatch!" << std::endl;
             return;
         }
-
+        std::cout<<"Dumping frame "<<"\n";
+      //  dumpFramebufferToFileWithTimeStamp(data,size);
+      //push fb and time stamp into queue;
+      //push_to_queue()
         struct nvnc_fb* fb = nvnc_fb_pool_acquire(fb_pool);
         if (!fb) {
             std::cerr << "Failed to acquire framebuffer from pool" << std::endl;
@@ -117,7 +177,7 @@ public:
 
 
    void captureScreen() {
-    std::string folderPath ="/home/prudhviraj/neatvnc_build/example/video_converte/frames";
+    std::string folderPath ="/home/lg/vnc/neat_vnc_local_server/temp_frames";
     static int frameCount = 1; // Keep track of the current frame
     char fileName[256];
     snprintf(fileName, sizeof(fileName), "%s/frame_%d.raw", folderPath.c_str(), frameCount);
@@ -153,10 +213,7 @@ public:
     struct aml* aml = nullptr; 
     std::thread eventLoopThread;
 
-    // X11 and XShm variables for screen capture
-    Display* xDisplay; 
-    XShmSegmentInfo* xShmInfo; 
-    unsigned char* shmBuffer; 
+   
 };
 
 int main() {
@@ -164,23 +221,48 @@ int main() {
    if (!vncServer.start()) {
         return EXIT_FAILURE;
     }
+    constexpr int fpsPrintIntervalSec = 1;
+    uint32_t m_fps=24;
+    std::chrono::milliseconds frame_interval(1000 / m_fps);
+    const std::chrono::milliseconds print_interval_ms(fpsPrintIntervalSec * 1000);
 
-    const auto frameInterval = std::chrono::milliseconds(42); // ~12 FPS
-   while (true) {
-        auto start_time = std::chrono::steady_clock::now();
+    auto start_time = std::chrono::steady_clock::now();
+    auto next_frame_time = start_time + frame_interval;
+    auto fps_interval_start = start_time;
+    uint32_t frame_count = 0;
+    int frames_in_interval = 0;
+    //start dump thread
 
-        // Capture the screen and update the framebuffer
+
+
+    while (true) {
         vncServer.captureScreen();
 
          // Update the framebuffer in the VNC server
          vncServer.updateFramebuffer(vncServer.getFramebuffer(), vncServer.width * vncServer.height * BYTES_PER_PIXEL);
 
-         auto end_time = std::chrono::steady_clock::now();
-         std::chrono::milliseconds frame_duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        frames_in_interval++;
+        frame_count++;
 
-         if (frame_duration < frameInterval) {
-             std::this_thread::sleep_for(frameInterval - frame_duration);
-         }
-     }
+        auto now = std::chrono::steady_clock::now();
+
+        if (now - fps_interval_start >= print_interval_ms) {
+            double elapsed_seconds = std::chrono::duration<double>(now - fps_interval_start).count();
+            double fps_actual = frames_in_interval / elapsed_seconds;
+            fps_interval_start = now;
+            frames_in_interval = 0;
+        }
+
+        std::this_thread::sleep_until(next_frame_time);
+
+        next_frame_time += frame_interval;
+
+        if (frame_count % (m_fps * 3600) == 0) {
+            start_time = std::chrono::steady_clock::now();
+            next_frame_time = start_time + frame_interval;
+            fps_interval_start = start_time;
+            frame_count = 0;
+        }
+    }
      return EXIT_SUCCESS;
 }
